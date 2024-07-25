b36e310d8956f5595954ab1d905a18844f581f28
Natan Nunes  Mendes <111541934+NatanNMendes@users.noreply.github.com>
2024-07-25 16:40:08 -0300

    Add files via upload

---
+++ santander-dev-week-2023/src/main/java/me/dio/domain/model/Card.java
@@ -0,0 +1,44 @@
+package me.dio.domain.model;
+
+import jakarta.persistence.*;
+
+import java.math.BigDecimal;
+
+@Entity(name = "tb_card")
+class Card {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private String number;
+
+    @Column(name = "avaliable_limit", precision = 13, scale = 2)
+    private BigDecimal limit;
+
+    // Getters and setters
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public BigDecimal getLimit() {
+        return limit;
+    }
+
+    public void setLimit(BigDecimal limit) {
+        this.limit = limit;
+    }
+}
